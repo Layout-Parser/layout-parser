ffc85effec4aa51dd58da0be5192b3f27063b024
lolipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-06-13 15:00:55 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="layoutparser",
+      version="0.0.1",
+      author="Zejiang Shen",
+      license="Apache",
+      url="https://github.com/Layout-Parser/layout-parser",
+      package_dir={"": "src"},
+      packages=find_packages("src"),
+      install_requires=[
+        "numpy"
+      ]
+      )
