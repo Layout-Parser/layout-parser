686855df6a917aa3fcc828b1f37bc616fd3a512c
AK391 <ahsengradio@gmail.com>
2021-04-15 01:46:08 -0400

    update desc

--- demo.py
+++ demo.py
@@ -5,15 +5,19 @@ model = lp.Detectron2LayoutModel('lp://PrimaLayout/mask_rcnn_R_50_FPN_3x/config'
 
 def lpi(img):
   layout = model.detect(img) # You need to load the image somewhere else, e.g., image = cv2.imread(...)
-  image = lp.draw_box(img, layout,) # With extra configurations
-  return image
+  return lp.draw_box(img, layout,) # With extra configurations
 
 inputs = gr.inputs.Image(type='pil', label="Original Image")
 outputs = gr.outputs.Image(type="pil",label="Output Image")
 
 title = "Layout Parser"
-description = "demo for OpenAI's CLIP. To use it, simply upload your image, or click one of the examples to load them and optionally add a text label seperated by commas to help clip classify the image better. Read more at the links below."
-article = "<p style='text-align: center'><a href='https://openai.com/blog/clip/'>CLIP: Connecting Text and Images</a> | <a href='https://github.com/openai/CLIP'>Github Repo</a></p>"
+description = "demo for Layout Parser. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
+article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2103.15348'>LayoutParser: A Unified Toolkit for Deep Learning Based Document Image Analysis</a> | <a href='https://github.com/Layout-Parser/layout-parser'>Github Repo</a></p>"
 
+examples = [
+    ['examples/data/example-table.jpeg'],
+    ['examples/data/paper-image.jpg']
+    
+]
 
-gr.Interface(lpi, inputs, outputs, title=title, description=description, article=article).launch(debug=True)
+gr.Interface(lpi, inputs, outputs, title=title, description=description, article=article).launch()
