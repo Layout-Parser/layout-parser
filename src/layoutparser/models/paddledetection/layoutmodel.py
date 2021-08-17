cf75a68f68556aa30c64624b4fe4f1dcb777e72b
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2021-08-17 01:44:52 -0400

    simplify the layoutmodel in paddledetection

--- src/layoutparser/models/paddledetection/layoutmodel.py
+++ src/layoutparser/models/paddledetection/layoutmodel.py
@@ -1,9 +1,11 @@
+from typing import List, Union, Dict, Any, Tuple
 import os
 from functools import reduce
+
 from PIL import Image
+import cv2
 import numpy as np
 
-from .preprocess import resize
 from .catalog import PathManager, LABEL_MAP_CATALOG
 from ..base_layoutmodel import BaseLayoutModel
 from ...elements import Rectangle, TextBlock, Layout
@@ -11,6 +13,29 @@ from ...elements import Rectangle, TextBlock, Layout
 
 __all__ = ["PaddleDetectionLayoutModel"]
 
+
+def _resize_image(
+    image: np.ndarray, target_size: Tuple[int, int]
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Args:
+        image (np.ndarray): image (np.ndarray)
+    Returns:
+        image (np.ndarray): processed image (np.ndarray)
+    """
+
+    origin_shape = image.shape[:2]
+
+    resize_h, resize_w = target_size
+    # im_scale_y: the resize ratio of Y
+    im_scale_y = resize_h / float(origin_shape[0])
+    # the resize ratio of X
+    im_scale_x = resize_w / float(origin_shape[1])
+
+    # resize image
+    image = cv2.resize(image, None, None, fx=im_scale_x, fy=im_scale_y, interpolation=2)
+    scale_factor = np.array([im_scale_y, im_scale_x]).astype("float32")
+    return image, scale_factor
 
 
 class PaddleDetectionLayoutModel(BaseLayoutModel):
@@ -35,9 +60,6 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
             Extra configuration passed to the PaddleDetection model configuration.
             Defaults to `{}`.
             Including arguments:
-            batch_size(:obj:`int`, optional):
-                Test batch size.
-                Defaults to 1.
             enable_mkldnn (:obj:`bool`, optional):
                 Whether use mkldnn to accelerate the computation.
                 Defaults to False.
@@ -68,36 +90,43 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
     ]
     DETECTOR_NAME = "paddledetection"
 
-    def __init__(self,
-                 config_path=None,
-                 model_path=None,
-                 label_map=None,
-                 enforce_cpu=False,
-                 extra_config={}):
+    def __init__(
+        self,
+        config_path=None,
+        model_path=None,
+        label_map=None,
+        enforce_cpu=False,
+        extra_config={},
+    ):
         if model_path is not None:
             model_dir = model_path
-        elif config_path is not None and config_path.startswith("lp://"):
-            prefix = "lp://"
+        elif config_path is not None and config_path.startswith(
+            "lp://"
+        ):  # TODO: Move "lp://" to a constant
             if label_map is None:
                 dataset_name = config_path.lstrip("lp://").split("/")[0]
                 label_map = LABEL_MAP_CATALOG[dataset_name]
-            model_name = config_path[len(prefix) :].split('/')[1]
             config_path = self._reconstruct_path_with_detector_name(config_path)
             model_dir = PathManager.get_local_path(config_path)
         else:
             raise Exception("Please set config_path or model_path first")
 
+        # TODO: rethink how to save store the default constants
         self.predictor = self.load_predictor(
             model_dir,
-            batch_size=extra_config.get('batch_size', 1),
             enforce_cpu=enforce_cpu,
-            enable_mkldnn=extra_config.get('enable_mkldnn', False),
-            thread_num=extra_config.get('thread_num', 10))
+            enable_mkldnn=extra_config.get("enable_mkldnn", False),
+            thread_num=extra_config.get("thread_num", 10),
+        )
 
-        self.threshold = extra_config.get('threshold', 0.5)
-        self.target_size = extra_config.get('target_size', [640,640])
-        self.pixel_mean = extra_config.get('pixel_mean', np.array([[[0.485, 0.456, 0.406]]]))
-        self.pixel_std = extra_config.get('pixel_std', np.array([[[0.229, 0.224, 0.225]]]))
+        self.threshold = extra_config.get("threshold", 0.5)
+        self.target_size = extra_config.get("target_size", [640, 640])
+        self.pixel_mean = extra_config.get(
+            "pixel_mean", np.array([[[0.485, 0.456, 0.406]]])
+        )
+        self.pixel_std = extra_config.get(
+            "pixel_std", np.array([[[0.229, 0.224, 0.225]]])
+        )
         self.label_map = label_map
 
     def _reconstruct_path_with_detector_name(self, path: str) -> str:
@@ -128,12 +157,13 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
                 return "lp://" + self.DETECTOR_NAME + "/" + path[len("lp://") :]
         return path
 
-    def load_predictor(self,
-                    model_dir,
-                    batch_size=1,
-                    enforce_cpu=False,
-                    enable_mkldnn=False,
-                    thread_num=10):
+    def load_predictor(
+        self,
+        model_dir,
+        enforce_cpu=False,
+        enable_mkldnn=False,
+        thread_num=10,
+    ):
         """set AnalysisConfig, generate AnalysisPredictor
         Args:
             model_dir (str): root path of __model__ and __params__
@@ -145,8 +175,11 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
         """
 
         config = self._inference.Config(
-            os.path.join(model_dir, 'inference.pdmodel'),
-            os.path.join(model_dir, 'inference.pdiparams'))
+            os.path.join(
+                model_dir, "inference.pdmodel"
+            ),  # TODO: Move them to some constants
+            os.path.join(model_dir, "inference.pdiparams"),
+        )
 
         if not enforce_cpu:
             # initial GPU memory(M), device ID
@@ -167,7 +200,6 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
                     print(
                         "The current environment does not support `mkldnn`, so disable mkldnn."
                     )
-                    pass                
 
         # disable print log when predict
         config.disable_glog_info()
@@ -179,7 +211,7 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
         return predictor
 
     def preprocess(self, image):
-        """ preprocess image
+        """preprocess image
 
         Args:
             image (np.ndarray): image (np.ndarray)
@@ -188,34 +220,31 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
         """
 
         # resize image by target_size and max_size
-        image, scale_factor = resize(image, self.target_size)
-        input_shape = np.array(image.shape[:2]).astype('float32')
+        image, scale_factor = _resize_image(image, self.target_size)
+        input_shape = np.array(image.shape[:2]).astype("float32")
         # normalize image
-        image =  (image / 255.0 - self.pixel_mean) / self.pixel_std
+        image = (image / 255.0 - self.pixel_mean) / self.pixel_std
         # transpose images
         image = image.transpose((2, 0, 1)).copy()
-        shape2 = np.array(image.shape[:2]).astype('float32')
 
         inputs = {}
-        inputs['image'] = np.array(image)[np.newaxis, :].astype('float32')
-        inputs['im_shape'] = np.array(input_shape)[np.newaxis, :].astype('float32')
-        inputs['scale_factor'] = np.array(scale_factor)[np.newaxis, :].astype('float32')
+        inputs["image"] = np.array(image)[np.newaxis, :].astype("float32")
+        inputs["im_shape"] = np.array(input_shape)[np.newaxis, :].astype("float32")
+        inputs["scale_factor"] = np.array(scale_factor)[np.newaxis, :].astype("float32")
         return inputs
 
-    def gather_output(self, np_boxes, np_masks):
+    def gather_output(self, np_boxes):
         """process output"""
         layout = Layout()
         results = []
         if reduce(lambda x, y: x * y, np_boxes.shape) < 6:
-            print('[WARNNING] No object detected.')
-            results = {'boxes': np.array([])}
+            print("[WARNING] No object detected.")
+            results = {"boxes": np.array([])}
         else:
             results = {}
-            results['boxes'] = np_boxes
-            if np_masks is not None:
-                results['masks'] = np_masks
+            results["boxes"] = np_boxes
 
-        np_boxes = results['boxes']
+        np_boxes = results["boxes"]
         expect_boxes = (np_boxes[:, 1] > self.threshold) & (np_boxes[:, 0] > -1)
         np_boxes = np_boxes[expect_boxes, :]
 
@@ -233,8 +262,7 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
 
         return layout
 
-    def detect(self,
-                image):
+    def detect(self, image):
         """Detect the layout of a given image.
 
         Args:
@@ -251,10 +279,10 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
             image = np.array(image)
 
         inputs = self.preprocess(image)
-
-        np_boxes, np_masks = None, None
+        
         input_names = self.predictor.get_input_names()
-        for i,input_name in enumerate(input_names):
+
+        for input_name in input_names:
             input_tensor = self.predictor.get_input_handle(input_name)
             input_tensor.copy_from_cpu(inputs[input_name])
 
@@ -263,6 +291,5 @@ class PaddleDetectionLayoutModel(BaseLayoutModel):
         boxes_tensor = self.predictor.get_output_handle(output_names[0])
         np_boxes = boxes_tensor.copy_to_cpu()
 
-        layout = self.gather_output(np_boxes, np_masks)
+        layout = self.gather_output(np_boxes)
         return layout
-        
