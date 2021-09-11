277f840e3da20688c9deae86f891cf46447b4d02
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2021-09-11 12:47:43 -0400

    minor fix

--- src/layoutparser/models/paddledetection/catalog.py
+++ src/layoutparser/models/paddledetection/catalog.py
@@ -1,4 +1,5 @@
-# Copyright 2021 The Layout Parser team. All rights reserved.
+# Copyright 2021 The Layout Parser team and Paddle Detection model 
+# contributors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
