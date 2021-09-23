6ff4f68bf1fc7a85521554610f9503d806d1c83b
lolipopshock <22512825+lolipopshock@users.noreply.github.com>
2021-09-23 11:40:41 -0400

    more backend specific tests

---
+++ tests_deps/test_only_detectron2.py
@@ -0,0 +1,31 @@
+# Copyright 2021 The Layout Parser team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cv2
+import pytest
+from layoutparser import Detectron2LayoutModel
+
+def test_only_effdet_model():
+
+    # When all the backeds are not installed, it should 
+    # elicit only ImportErrors
+    
+    config = "lp://PubLayNet/ppyolov2_r50vd_dcn_365e/config"
+    model = Detectron2LayoutModel(config)
+    image = cv2.imread("tests/fixtures/model/test_model_image.jpg")
+    layout = model.detect(image)
+    
+    with pytest.raises(ImportError):
+        from layoutparser import EfficientDetLayoutModel
+        from layoutparser import PaddleDetectionLayoutModel
