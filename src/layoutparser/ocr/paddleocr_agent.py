3feb2a1d5afed0ff8823ede60a2ca606638ebcb2
an1018 <614803115@qq.com>
2021-09-27 11:47:45 +0800

    add paddleocr

--- src/layoutparser/ocr/paddleocr_agent.py
+++ src/layoutparser/ocr/paddleocr_agent.py
@@ -12,15 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import io
-import csv
 import pickle
+import cv2
 import numpy as np
 
-import pandas as pd
-
-from .base import BaseOCRAgent, BaseOCRElementType
-from ..io import load_dataframe
+from .base import BaseOCRAgent
 from ..file_utils import is_paddleocr_available
 
 if is_paddleocr_available():
@@ -35,7 +31,7 @@ class PaddleOCRAgent(BaseOCRAgent):
 
     DEPENDENCIES = ["paddleocr"]
 
-    def __init__(self, languages="en", use_gpu=True, use_angle_cls=False, det=True, rec=True, cls=False, **kwargs):
+    def __init__(self, languages="en", use_gpu=False, use_angle_cls=False):
         """Create a Tesseract OCR Agent.
 
         Args:
@@ -49,29 +45,42 @@ class PaddleOCRAgent(BaseOCRAgent):
         self.lang = languages
         self.use_gpu = use_gpu
         self.use_angle_cls = use_angle_cls
-        self.configs = kwargs
         self.ocr = paddleocr.PaddleOCR(use_gpu=self.use_gpu, use_angle_cls=self.use_angle_cls, lang=self.lang)
-   
-    def resized_long(self, image, target=480):
+
+    def resized_long(self, image, target_size):
         shape = image.shape
-        if max(image.shape[0], image.shape[1]) >= target:
+        if max(image.shape[0], image.shape[1]) >= target_size:
             return image
         if shape[0] >= shape[1]:
-            ratio = 1.0 * target / shape[0]
-            out = [int(shape[1] * ratio), target]
+            ratio = 1.0 * target_size / shape[0]
+            out = (int(shape[1] * ratio), target_size)
         else:
-            ratio = 1.0 * target / shape[1]
-            out = [target, int(shape[0] * ratio)]
+            ratio = 1.0 * target_size / shape[1]
+            out = (target_size, int(shape[0] * ratio))
         return cv2.resize(image, out)
 
-    def pad_img_to_longer_edge(self, image):
+    def pad_img_to_longer_edge(self, image, padding_value=127):
         max_shape = max(image.shape[0], image.shape[1])
-        out_img = np.ones([max_shape, max_shape, 3]) * 127
+        out_img = np.ones([max_shape, max_shape, 3]) * padding_value
         out_img[:image.shape[0], :image.shape[1], :image.shape[2]] = image
         return out_img
 
+    def _detect(self, img_content, target_size, padding_value,
+            det, rec, cls, threshold):
+        res = {}
+        img_content = self.resized_long(img_content, target_size)
+        img_content = self.pad_img_to_longer_edge(img_content, padding_value)
+        result = self.ocr.ocr(img_content, det=det, rec=rec, cls=cls)
+        text = []
+        for line in result:
+            if line[1][1]>threshold:
+                text.append(line[1][0])
+        res["text"] = '\n'.join(text)
+        return res
+
     def detect(
-        self, image, det=True, rec=True, cls=True,
+        self, image, target_size=480, padding_value=127,
+        det=True, rec=True, cls=True, threshold=0.5,
         return_response=False, return_only_text=True
     ):
         """Send the input image for OCR.
@@ -79,15 +88,24 @@ class PaddleOCRAgent(BaseOCRAgent):
         Args:
             image (:obj:`np.ndarray` or :obj:`str`):
                 The input image array or the name of the image file
-            det (:obj:`bool`, optional): 
+            target_size (:obj:`int`, optional):
+                The size of the longest side after resize.
+                Default to `480`.
+            padding_value (:obj:`int`, optional):
+                The padding value will apply to get a square image.
+                Default to `127`.
+            det (:obj:`bool`, optional):
                 use text detection or not, if false, only rec will be exec.
                 Default to `True`.
-            rec (:obj:`bool`, optional): 
-                Use text recognition or not, if false, only det will be exec. 
+            rec (:obj:`bool`, optional):
+                Use text recognition or not, if false, only det will be exec.
                 Default to `True`.
             cls (:obj:`bool`, optional):
-                Use 180 degree rotation text recognition or not. 
+                Use 180 degree rotation text recognition or not.
                 Default to `True`.
+            threshold (:obj:`float`, optional):
+                Filter the recognition results with recognition scores less than threshold.
+                Default to '0.5'.
             return_response (:obj:`bool`, optional):
                 Whether directly return all output (string and boxes
                 info) from Tesseract.
@@ -95,18 +113,17 @@ class PaddleOCRAgent(BaseOCRAgent):
             return_only_text (:obj:`bool`, optional):
                 Whether return only the texts in the OCR results.
                 Defaults to `False`.
-        """ 
-        image = self.resized_long(image)
-        image = self.pad_img_to_longer_edge(image)
-        res = self.ocr.ocr(image, det=det, rec=rec, cls=cls)
+        """
+
+        res = self._detect(image, target_size, padding_value, det, rec, cls, threshold)
 
         if return_response:
             return res
-        
-        if return_only_text:
-            return ['\n'.join(line[1][0] for line in res)]
 
-        return ['\n'.join(line[1][0] for line in res)]
+        if return_only_text:
+            return res["text"]
+
+        return res["text"]
 
     @staticmethod
     def load_response(filename):
